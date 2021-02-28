a42b28f1f6c58d98e2072f53f672a4b2c5a10789
Martin Falang <falang.martin@gmail.com>
2021-02-28 13:47:27 +0100

    Implement gaens crypto helper functions
    Implement functions for helping with using the crypto library following
    GAENS specifications.
    
    The functions implemented (and assumed needed by the BLE module) are
    (in the same order as they appear in the file):
    
    - Function for getting the current rolling proximity identifier (RPI)
    - Function for getting the current temporary exposure key (TEK) and the
    timestamp from which that TEK key i valid
    - Function for updating the current RPI which should be called every
    time the BLE address i changed
    - Function for updating the encryption keys (TEK, RPIK, AEMK) which
    should be called once every 24 hours when the current TEK is no longer
    valid
    - Function for encrypting metadata used in the advertising payload
    - Function for checking if the current BLE address has expired and must
    be changed
    - Function for checking if the current TEK has expired and thus must
    be changed
    
    Files modified:
    - CMakeLists.txt: Add gaens to build targets
    - src/gaens/gaens.c: Implement gaens crypto helper functions
    - src/gaens/gaens.h: Declare gaens crypto helper functions
    
    CT-181

--- src/gaens/gaens.c
+++ src/gaens/gaens.c
@@ -1,20 +1,44 @@
 
+////////////////////////////////////////////////////////////////////////////////
+// Includes
+////////////////////////////////////////////////////////////////////////////////
+
 #include "gaens.h"
-#include <string.h>
 #include <logging/log.h>
+#include <string.h>
 
-#define LOG_MODULE_NAME crypto
-LOG_MODULE_REGISTER(crypto);
+////////////////////////////////////////////////////////////////////////////////
+// Defines
+////////////////////////////////////////////////////////////////////////////////
 
-static uint32_t _tek_validity_timestamp = 0;
+#define LOG_MODULE_NAME gaens
+LOG_MODULE_REGISTER(gaens);
+
+////////////////////////////////////////////////////////////////////////////////
+// Private variables
+////////////////////////////////////////////////////////////////////////////////
+
+static uint32_t _ble_addr_change_timestamp = 0;
+static uint32_t _current_tek_valid_from = 0;
 static uint8_t _current_tek[TEK_LENGTH] = {0};
 static uint8_t _current_rpi[RPI_LENGTH] = {0};
 static uint8_t _current_rpik[RPIK_LENGTH] = {0};
 static uint8_t _current_aemk[AEMK_LENGTH] = {0};
 
+////////////////////////////////////////////////////////////////////////////////
+// Public functions
+////////////////////////////////////////////////////////////////////////////////
+
 int gaens_get_rpi(uint8_t *rpi)
 {
     memcpy(rpi, _current_rpi, RPI_LENGTH);
+    return 0;
+}
+
+int gaens_get_tek(uint8_t *tek, uint32_t *tek_timestamp)
+{
+    memcpy(tek, _current_tek, TEK_LENGTH);
+    *tek_timestamp = _current_tek_valid_from;
     return 0;
 }
 
@@ -28,12 +52,74 @@ int gaens_update_rpi(void)
     return 0;
 }
 
-int gaens_update_tek(void)
+int gaens_update_keys(void)
 {
-    if (crypto_tek(_current_tek, TEK_LENGTH, &_tek_validity_timestamp) < 0)
+    if (crypto_tek(_current_tek, TEK_LENGTH, &_current_tek_valid_from) < 0)
     {
         LOG_ERR("Failed to update temporary exposure key");
         return -1;
     }
+
+    if (crypto_rpik(_current_tek, TEK_LENGTH, _current_rpik, RPIK_LENGTH) < 0)
+    {
+        LOG_ERR("Failed to update rolling proximity identifier key");
+        return -1;
+    }
+
+    if (crypto_aemk(_current_tek, TEK_LENGTH, _current_aemk, AEMK_LENGTH) < 0)
+    {
+        LOG_ERR("Failed to update associated encrypted metadata key");
+        return -1;
+    }
+
+    return 0;
+}
+
+int gaens_encrypt_metadata(const uint8_t *metadata, const uint8_t metadata_len,
+                           uint8_t *aem)
+{
+    if (crypto_aem(_current_aemk, _current_rpi, metadata, metadata_len, aem) <
+        0)
+    {
+        LOG_ERR("Failed to encrypt metadata");
+        return -1;
+    }
+
+    return 0;
+}
+
+int gaens_ble_addr_expired(void)
+{
+    uint32_t en_interval_num;
+    if (crypto_en_interval_number(&en_interval_num) < 0)
+    {
+        LOG_ERR("Failed to get exposure notification interval number");
+        return -1;
+    }
+
+    // When the interval number increases by 1, 10 minutes have passed, i.e. a
+    // timeout has occured
+    if (en_interval_num > _ble_addr_change_timestamp)
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+int gaens_tek_expired(void)
+{
+    uint32_t en_interval_num;
+    if (crypto_en_interval_number(&en_interval_num) < 0)
+    {
+        LOG_ERR("Failed to get exposure notification interval number");
+        return -1;
+    }
+
+    if (en_interval_num >= _current_tek_valid_from + TEK_ROLLING_PERIOD)
+    {
+        return 1;
+    }
+
     return 0;
 }
--- src/gaens/gaens.h
+++ src/gaens/gaens.h
@@ -1,7 +1,110 @@
 #ifndef GAENS_H
 #define GAENS_H
 
-#include <stdint.h>
+////////////////////////////////////////////////////////////////////////////////
+// Includes
+////////////////////////////////////////////////////////////////////////////////
+
 #include "crypto.h"
+#include <stdint.h>
+
+////////////////////////////////////////////////////////////////////////////////
+// Defines
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @brief Length (in bytes) of the associated encrypted metadata as specified 
+ * in the GAENS Bluetooth specification.
+ */
+#define AEM_LENGTH 20
+
+////////////////////////////////////////////////////////////////////////////////
+// Public function declarations
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @brief Get the current rolling proximity identifier (RPI).
+ * 
+ * @param rpi Pointer to store the current RPI in. Must be of length 
+ * RPI_LENGTH
+ * @return int 0 on success, negative otherwise
+ */
+int gaens_get_rpi(uint8_t *rpi);
+
+/**
+ * @brief Get the current temporary exposure key (TEK) and the timestamp from
+ * which it is valid.
+ * 
+ * @param tek Pointer to store the current TEK in. Must be of length RPI_LENGTH
+ * @param tek_timestamp Timestamp from which current TEK is valid (in units of
+ * 10 minutes)
+ * @return int 0 on success, negative otherwise
+ */
+int gaens_get_tek(uint8_t *tek, uint32_t *tek_timestamp);
+
+/**
+ * @brief Derive a new rolling proximity identifier (RPI) and store this for
+ * future use. The new RPI can be obtained by calling the function 
+ * @c gaens_get_rpi.
+ * 
+ * @note This function should be called every time the function
+ * @c gaens_ble_addr_expired returns 1, which happens once every 10 
+ * minutes.
+ * 
+ * @return int 0 on success, negative otherwise
+ */
+int gaens_update_rpi(void);
+
+/**
+ * @brief Updates the temporary exposure key (TEK), rolling proximity identifier
+ * key (RPIK), and associated encrypted metadata key (AEMK). The current TEK and
+ * timestamp can be obtained by calling the function @c gaens_get_tek, while
+ * the current RPIK and AEMK are internal to this module and cannot be 
+ * extracted.
+ * 
+ * @note This function should be called every time the function 
+ * @c gaens_tek_expired returns 1, which happens once every 24 hours.
+ * 
+ * @return int 0 on success, negative otherwise
+ */
+int gaens_update_keys(void);
+
+/**
+ * @brief Encrypt metadata to be stored in exposure notification 
+ * advertisement packets.
+ * 
+ * @param metadata Pointer to metadata to encrypt
+ * @param metadata_len Length of metadata to encrypt (should be @c AEM_LENGTH)
+ * @param aem Pointer to store the associated encrypted metadata in (should be
+ * of length @c AEM_LENGTH)
+ * @return int 0 on success, negative otherwise
+ */
+int gaens_encrypt_metadata(const uint8_t *metadata, const uint8_t metadata_len,
+                           uint8_t *aem);
+
+/**
+ * @brief Check if 10 minutes have passed since the last time the Bluetooth
+ * address was changed, meaning the address must be changed. 
+ * 
+ * @note When this function returns 1, the current RPI will not be valid and 
+ * thus the function @c gaens_update_rpi must be called in order to create a new
+ * valid RPI.
+ * 
+ * @return int 0 if less than 10 minutes have passed, 1 if 10 minutes or more
+ * have passed, negative on error
+ */
+int gaens_ble_addr_expired(void);
+
+/**
+ * @brief Check if temporary exposure key (TEK) has expired, i.e. 24 hours have 
+ * passed since the last TEK key was generated.
+ * 
+ * @note When this function returns 1, the current TEK, RPIK and AEMK are no 
+ * longer valid and thus the function @c gaens_update_keys must be called in 
+ * order to update these keys.
+ * 
+ * @return int 0 if TEK has not expired, 1 if TEK has expired, negative on error
+ */
+int gaens_tek_expired(void);
 
 #endif // GAENS_H
