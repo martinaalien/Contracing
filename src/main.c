5de88384446b3040f6da0dc1b30ecc3301b96139
Martin Aalien <martin.aalien@outlook.com>
2021-04-19 19:53:27 +0000

    Fix simultaneous GAENS and WENS advertising
    This commit enables simultaneous GAENS and WENS advertising without
    having to use timers and wake up the cores.
    
    Files modified:
    - prj.conf: Add necessary configs, and cleaning
    - src/ble/advertise.c: Fix simultaneous GAENS and WENS advertising
    - src/ble/advertise.h: Fix simultaneous GAENS and WENS advertising
    - src/main.c: Update main
    
    CT-172

--- src/main.c
+++ src/main.c
@@ -2,7 +2,12 @@
 // Includes
 ////////////////////////////////////////////////////////////////////////////////
 
+#include "ble/advertise.h"
 #include "ble/ble.h"
+#include "ble/scan.h"
+#include "ble/services/wens/wens.h"
+#include "records/extmem.h"
+#include "records/storage.h"
 #include <stdio.h>
 #include <string.h>
 
@@ -23,7 +28,39 @@ LOG_MODULE_REGISTER(main);
 
 void main(void)
 {
+    int err;
+
     LOG_INF("It's alive!\n");
 
-    ble_init();
+    err = ble_init();
+    if (err)
+    {
+        LOG_ERR("Failed to initialize BLE");
+    }
+
+    err = extmem_init();
+    if (err)
+    {
+        LOG_ERR("Failed to initialize external memory");
+    }
+
+    err = advertise_start();
+    if (err)
+    {
+        LOG_ERR("Failed to start advertising");
+    }
+
+    err = scan_start();
+    if (err)
+    {
+        LOG_ERR("Failed to start scanning");
+    }
+
+    // extmem_erase(0, EXTMEM_SUBSECTOR_SIZE);
+
+    // uint8_t rp[16] = {1,2,3,4,5,6,7,8,9,1,2,3,4,5,5, 6};
+    // storage_write_entry(0x22223333, rp, 0xaa, 0xdd);
+    // storage_write_entry(0x22223333, rp, 0xaa, 0xdd);
+    // storage_write_entry(0x22223333, rp, 0xaa, 0xdd);
+    // storage_read_block();
 }
