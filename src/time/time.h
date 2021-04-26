e03b268d874b1f80ad9a53ffc3346f667f2f0172
Martin Aalien <martin.aalien@outlook.com>
2021-04-26 18:12:35 +0000

    Add time library
    This commit creates a time library for getting and setting the time.
    
    Files added:
    - src/time/time.c: Add functions for setting and getting the time
    - src/time/time.h: Add functions for setting and getting the time
    
    CT-166

---
+++ src/time/time.h
@@ -0,0 +1,32 @@
+#ifndef TIME_H
+#define TIME_H
+
+////////////////////////////////////////////////////////////////////////////////
+// Includes
+////////////////////////////////////////////////////////////////////////////////
+
+#include <stdint.h>
+
+////////////////////////////////////////////////////////////////////////////////
+// Function declarations
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @brief Set the current time.
+ * 
+ * @param current_time Time you want to set.
+ *
+ * @return int 0 on success, negative otherwise
+ */
+int set_current_time(uint32_t current_time);
+
+/**
+ * @brief Get the current time.
+ * 
+ * @param time Pointer to store the time in.
+ * 
+ * @return int 0 on success, negative otherwise
+ */
+int get_current_time(uint32_t *time);
+
+#endif
