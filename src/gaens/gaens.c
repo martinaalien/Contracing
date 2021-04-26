3d7e7a3a6b38833c4086d436305948a5c77f2910
Martin Aalien <martin.aalien@outlook.com>
2021-04-26 18:36:47 +0000

    Update GAENS service data accoring to specs
    This commits enables the GAENS service data to be changed at
    a random interval greater than 10 minutes and less than 20 minutes.
    This also updates the advertising packets.
    
    Files modified:
    - src/gaens/gaens.c: Update GAENS service data accoring to specs
    - src/gaens/gaens.h: Update GAENS service data accoring to specs
    
    CT-166

--- src/gaens/gaens.c
+++ src/gaens/gaens.c
@@ -4,7 +4,11 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include "gaens.h"
+#include "../ble/advertise.h"
+
+/* Zephyr includes */
 #include <logging/log.h>
+#include <random/rand32.h>
 #include <string.h>
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -13,6 +17,21 @@
 
 #define LOG_MODULE_NAME gaens
 LOG_MODULE_REGISTER(gaens);
+
+#define AEM_VERSONING                                                          \
+    0b01000000 // Defined in GAENS                                             \
+               // Bluetooth specification
+#define AEM_TRANSMIT_POWER                                                     \
+    0b0         // Defined in GAENS                                            \
+                // Bluetooth specification. 0dBm
+#define RFU 0b0 // Ready for Future Use
+
+#define LOWER_RANDOM_ROTATION_INTERVAL                                         \
+    601 // Lower limit is greater                                              \
+        // than 10 minutes
+#define UPPER_RANDOM_ROTATION_INTERVAL                                         \
+    1199 // Upper limit is less                                                \
+         //than 20 minutes
 
 ////////////////////////////////////////////////////////////////////////////////
 // Private variables
@@ -25,23 +44,84 @@ static uint8_t current_rpi[RPI_LENGTH] = {0};
 static uint8_t current_rpik[RPIK_LENGTH] = {0};
 static uint8_t current_aemk[AEMK_LENGTH] = {0};
 
+/* Metadata to be encrypted and advertised */
+static uint8_t metadata[AEM_LENGTH] = {RFU, RFU, AEM_TRANSMIT_POWER,
+                                       AEM_VERSONING};
+
+////////////////////////////////////////////////////////////////////////////////
+// Private function declarations
+////////////////////////////////////////////////////////////////////////////////
+
+static int _gaens_random_rotation_interval(uint32_t *interval);
+
+static void _rotate_rpi_handler(struct k_work *unused);
+K_WORK_DEFINE(_rotate_rpi_work, _rotate_rpi_handler);
+
+static void _rpi_rotation_timer_handler(struct k_timer *unused);
+K_TIMER_DEFINE(_rpi_rotation_timer, _rpi_rotation_timer_handler, NULL);
+
 ////////////////////////////////////////////////////////////////////////////////
 // Public functions
 ////////////////////////////////////////////////////////////////////////////////
 
 int gaens_init(void)
 {
+    // Initialize crypto library
     if (crypto_init() < 0)
     {
         LOG_ERR("Failed to init crypto module");
         return -1;
     }
 
+    // Initiate keys
     if (gaens_update_keys() < 0)
     {
         LOG_ERR("Failed to initiate keys");
         return -1;
     }
+
+    // Initiate RPI
+    if (gaens_update_rpi() < 0)
+    {
+        LOG_ERR("Failed to update initial RPI");
+        return -1;
+    }
+
+    // Fetch new RPI
+    uint8_t rpi[RPI_LENGTH] = {0};
+    if (gaens_get_rpi(rpi) < 0)
+    {
+        LOG_ERR("Failed to fetch initial RPI");
+        return -1;
+    }
+
+    // Encrypt AEM
+    uint8_t aem[AEM_LENGTH];
+    if (gaens_encrypt_metadata(metadata, AEM_LENGTH, aem) < 0)
+    {
+        LOG_ERR("Failed to encrypt initial AEM");
+        return -1;
+    }
+
+    // Change advertise data
+    if (advertise_change_gaens_service_data(rpi, RPI_LENGTH, aem, AEM_LENGTH) <
+        0)
+    {
+        LOG_ERR("Failed to update initial advertise data");
+        return -1;
+    }
+
+    // Get random rotation interval between 10 and 20 minutes
+    uint32_t random_time;
+    if (_gaens_random_rotation_interval(&random_time) < 0)
+    {
+        LOG_ERR("Failed to fetch random rotation interval");
+        return -1;
+    }
+
+    // Start a timer that will trigger in random_time
+    k_timer_start(&_rpi_rotation_timer, K_SECONDS(random_time),
+                  K_SECONDS(random_time));
 
     return 0;
 }
@@ -129,13 +209,13 @@ int gaens_encrypt_metadata(const uint8_t *metadata, const uint8_t metadata_len,
     return 0;
 }
 
-int gaens_decrypt_metadata(const uint8_t *aem, const uint8_t aem_len, 
-                            uint8_t *decrypted_aem)
+int gaens_decrypt_metadata(const uint8_t *aem, const uint8_t aem_len,
+                           uint8_t *decrypted_aem)
 {
     uint8_t rpi_copy[RPI_LENGTH];
     memcpy(rpi_copy, current_rpi, RPI_LENGTH);
 
-    if (crypto_aem_decrypt(aem, aem_len, current_aemk, rpi_copy, 
+    if (crypto_aem_decrypt(aem, aem_len, current_aemk, rpi_copy,
                            decrypted_aem) < 0)
     {
         LOG_ERR("Failed to decrypt AEM");
@@ -181,4 +261,115 @@ int gaens_tek_expired(void)
     }
 
     return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Private functions
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @brief Returns a random time greater than 10 minutes and less
+ * than 20 minutes.
+ * 
+ * @param interval A pointer to the random time interval in seconds.
+ * 
+ * @return int 0 on success, negative otherwise.
+ */
+static int _gaens_random_rotation_interval(uint32_t *interval)
+{
+    uint32_t random_number = 0;
+
+    if (sys_csrand_get(&random_number, sizeof(random_number)) < 0)
+    {
+        LOG_ERR("Failed to get random number");
+        return -1;
+    }
+
+    *interval = (random_number % (UPPER_RANDOM_ROTATION_INTERVAL -
+                                  LOWER_RANDOM_ROTATION_INTERVAL + 1)) +
+                LOWER_RANDOM_ROTATION_INTERVAL;
+
+    return 0;
+}
+
+/**
+ * @brief Work handler for changing the RPI, AEM and update advertise data. 
+ * 
+ * @param unused Not in use, but required.
+ */
+static void _rotate_rpi_handler(struct k_work *unused)
+{
+    // Stop advertising
+    if (advertise_stop() < 0)
+    {
+        LOG_ERR("Failed to pause the advertising");
+    }
+
+    // Stop the timer
+    k_timer_stop(&_rpi_rotation_timer);
+
+    // Check if the Temporary Exposure Key has expired
+    if (gaens_tek_expired() == 1)
+    {
+        if (gaens_update_keys() < 0)
+        {
+            LOG_ERR("Failed to update TEK");
+        }
+    }
+
+    // Update RPI
+    if (gaens_update_rpi() < 0)
+    {
+        LOG_ERR("Failed to update the RPI");
+    }
+
+    // Fetch new RPI
+    uint8_t rpi[RPI_LENGTH] = {0};
+    if (gaens_get_rpi(rpi) < 0)
+    {
+        LOG_ERR("Failed to fetch the RPI");
+    }
+
+    // Encrypt AEM
+    uint8_t aem[AEM_LENGTH];
+    if (gaens_encrypt_metadata(metadata, AEM_LENGTH, aem) < 0)
+    {
+        LOG_ERR("Failed to encrypt the metadata");
+    }
+
+    // Change advertise data
+    if (advertise_change_gaens_service_data(rpi, RPI_LENGTH, aem, AEM_LENGTH) <
+        0)
+    {
+        LOG_ERR("Failed to change the gaens service data to advertise");
+    }
+
+    // Get random rotation interval between 10 and 20 minutes
+    uint32_t random_time;
+    if (_gaens_random_rotation_interval(&random_time) < 0)
+    {
+        LOG_ERR("Failed to fetch random rotation interval");
+    }
+
+    // Start a timer that will trigger in random_time
+    k_timer_start(&_rpi_rotation_timer, K_SECONDS(random_time),
+                  K_SECONDS(random_time));
+
+    // Start advertising
+    if (advertise_start() < 0)
+    {
+        LOG_ERR("Failed to resume advertising");
+    }
+
+    LOG_INF("Successfully updated RPI and AEM");
+}
+
+/**
+ * @brief Handler for submitting work for rotating the RPI.
+ * 
+ * @param unused Not in use, but required.
+ */
+static void _rpi_rotation_timer_handler(struct k_timer *unused)
+{
+    k_work_submit(&_rotate_rpi_work);
 }
