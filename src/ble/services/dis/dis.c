69804d89b3afc19f0b148a0e7f0ad93e6a3740a4
Martin Aalien <martin.aalien@outlook.com>
2021-05-29 14:07:53 +0000

    Add implementation of DIS
    Add implementation of Device Information Service.
    
    Files added:
    - src/ble/services/dis/dis.c: Add implementation of DIS
    - src/ble/services/dis/dis.h: Add implementation of DIS
    
    Files deleted:
    - src/ble/services/dis/.gitkeep: No longer needed
    
    CT-152

---
+++ src/ble/services/dis/dis.c
@@ -0,0 +1,82 @@
+////////////////////////////////////////////////////////////////////////////////
+// Includes
+////////////////////////////////////////////////////////////////////////////////
+
+/* Zephyr includes */
+#include <errno.h>
+#include <init.h>
+#include <stddef.h>
+#include <string.h>
+#include <zephyr.h>
+#include <zephyr/types.h>
+
+#include <settings/settings.h>
+
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/conn.h>
+#include <bluetooth/gatt.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/uuid.h>
+
+////////////////////////////////////////////////////////////////////////////////
+// Defines
+////////////////////////////////////////////////////////////////////////////////
+
+// #define ENABLE_DIS_SETTINGS
+
+#define BT_DBG_ENABLED  IS_ENABLED(CONFIG_BT_DEBUG_SERVICE)
+#define LOG_MODULE_NAME bt_dis
+#include "common/log.h"
+
+#define BT_DIS_MODEL_REF      "nRF52833_QIAA"
+#define BT_DIS_MANUF_REF      "Manufacturer"
+#define BT_DIS_FW_REV_STR_REF "Zephyr Firmware"
+#define BT_DIS_HW_REV_STR_REF "Zephyr Hardware"
+
+////////////////////////////////////////////////////////////////////////////////
+// Private function declarations
+////////////////////////////////////////////////////////////////////////////////
+
+static ssize_t read_str(struct bt_conn *conn, const struct bt_gatt_attr *attr,
+                        void *buf, uint16_t len, uint16_t offset);
+
+////////////////////////////////////////////////////////////////////////////////
+// Service define
+////////////////////////////////////////////////////////////////////////////////
+
+BT_GATT_SERVICE_DEFINE(
+    dis_svc, BT_GATT_PRIMARY_SERVICE(BT_UUID_DIS),
+
+    BT_GATT_CHARACTERISTIC(BT_UUID_DIS_MODEL_NUMBER, BT_GATT_CHRC_READ,
+                           BT_GATT_PERM_READ, read_str, NULL, BT_DIS_MODEL_REF),
+    BT_GATT_CHARACTERISTIC(BT_UUID_DIS_MANUFACTURER_NAME, BT_GATT_CHRC_READ,
+                           BT_GATT_PERM_READ, read_str, NULL, BT_DIS_MANUF_REF),
+    BT_GATT_CHARACTERISTIC(BT_UUID_DIS_FIRMWARE_REVISION, BT_GATT_CHRC_READ,
+                           BT_GATT_PERM_READ, read_str, NULL,
+                           BT_DIS_FW_REV_STR_REF),
+    BT_GATT_CHARACTERISTIC(BT_UUID_DIS_HARDWARE_REVISION, BT_GATT_CHRC_READ,
+                           BT_GATT_PERM_READ, read_str, NULL,
+                           BT_DIS_HW_REV_STR_REF), );
+
+////////////////////////////////////////////////////////////////////////////////
+// Private functions
+////////////////////////////////////////////////////////////////////////////////'
+
+/**
+ * @brief Function for reading the characteristic.
+ * 
+ * @param conn Connection object.
+ * @param attr Attribute to read.
+ * @param buf Buffer to store the value.
+ * @param len Buffer length.
+ * @param offset Start offset.
+ * 
+ * @return ssize_t number of bytes read in case of success or negative 
+ * values in case of error.
+ */
+static ssize_t read_str(struct bt_conn *conn, const struct bt_gatt_attr *attr,
+                        void *buf, uint16_t len, uint16_t offset)
+{
+    return bt_gatt_attr_read(conn, attr, buf, len, offset, attr->user_data,
+                             strlen(attr->user_data));
+}
---
+++ src/ble/services/dis/dis.h
@@ -0,0 +1,8 @@
+/** @file
+ *  @brief Device Information Service
+ */
+
+#ifndef DIS_H
+#define DIS_H
+
+#endif // DIS_H
